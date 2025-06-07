5bf6accf11af13163d31780503f87b60d5bf6bf7
yakoobkhedri <yakkobkhedrii@gmail.com>
2025-06-08 01:18:13 +0330

    first commit

--- src/js/table.js
+++ src/js/table.js
@@ -1,10 +1,19 @@
-window.addEventListener('load',function () {
-  const table= document.querySelector('.editor-section').querySelector('table');
-  const newDiv = document.createElement("div");
-  table.parentNode.insertBefore(newDiv,table);
-  newDiv.appendChild(table);
-  newDiv.classList.add('overflow-x-auto');
-  newDiv.classList.add('shadow-13');
-  newDiv.classList.add('rounded-[24px]');
-  newDiv.classList.add('px-6');
-})
+window.addEventListener('load', function() {
+  const editorSection = document.querySelector('.editor-section');
+  const tables = editorSection.querySelectorAll('table');
+  
+  tables.forEach(table => {
+    // ایجاد div والد اصلی با کلاس‌های مورد نظر
+    const outerDiv = document.createElement("div");
+    outerDiv.classList.add('mt-6', 'rounded-[15px]', 'sm:rounded-[30px]', 'bg-[#E8EDF3]', 'pb-[30px]');
+    
+    // ایجاد div داخلی برای جدول
+    const innerDiv = document.createElement("div");
+    innerDiv.classList.add('overflow-x-auto', 'shadow-13', 'rounded-[24px]', 'px-6');
+    
+    // جایگزینی جدول با ساختار جدید
+    table.parentNode.insertBefore(outerDiv, table);
+    outerDiv.appendChild(innerDiv);
+    innerDiv.appendChild(table);
+  });
+});
